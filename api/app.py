07745bb92666274f80bcc0ee5731e1b545fe1a7b
Neel Patel <neel.patel6573@gmail.com>
2020-10-11 15:53:22 -0400

    regenerates pkl file with joblib

--- api/app.py
+++ api/app.py
@@ -1,24 +1,25 @@
 from flask import Flask, request, jsonify
-from sklearn.externals import joblib
 import numpy as np
+import pickle
 
 app = Flask(__name__)
 
-@app.route("/predict",methods=["GET"])
+@app.route("/predict",methods = ["GET"])
 def predict_acceptance_rate():
-    data=request.json
-    keys=data.keys()
-    required=["GRE Score","TOEFL Score","CGPA","University Rating","SOP"]
+    data = request.json
+    keys = data.keys()
+    required = ["GRE Score","TOEFL Score","CGPA","University Rating","SOP"]
     for i in required:
         if i not in keys:
-            message="Please include all the data"
-            return jsonify(message=message),200
-    a=[ j for i,j in data.items()]
-    b=[]
+            message = "Please include all the data"
+            return jsonify(message = message),200
+    a = [ j for i,j in data.items()]
+    b = []
     b.append(a)
-    x=model.predict(b)
-    return jsonify(acceptance=x[0])
+    x = model.predict(b)
+    return jsonify(acceptance = x[0])
 
-if __name__=="__main__":
-    model=joblib.load("usa-graduate-admission.pkl")
+if __name__ == "__main__":
+    file = open("usa-graduate-admission.pkl","rb")
+    model = pickle.load(file)
     app.run()
