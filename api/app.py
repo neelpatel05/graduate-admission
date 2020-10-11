78bc3e84cc03f11dda67179631da2e2e604798c1
Neel Patel <neel.patel6573@gmail.com>
2020-10-11 16:23:41 -0400

    creates two .sav models

--- api/app.py
+++ api/app.py
@@ -4,9 +4,12 @@ import pickle
 
 app = Flask(__name__)
 
-@app.route("/predict",methods = ["GET"])
+@app.route("/",methods = ["GET"])
 def predict_acceptance_rate():
     data = request.json
+    if data == None:
+        message = "Please include all the data"
+        return jsonify(message = message),200
     keys = data.keys()
     required = ["GRE Score","TOEFL Score","CGPA","University Rating","SOP"]
     for i in required:
@@ -14,12 +17,12 @@ def predict_acceptance_rate():
             message = "Please include all the data"
             return jsonify(message = message),200
     a = [ j for i,j in data.items()]
-    b = []
-    b.append(a)
+    print(a)
+    b = [[310,100,8,2,3,5]]
     x = model.predict(b)
     return jsonify(acceptance = x[0])
 
 if __name__ == "__main__":
-    file = open("usa-graduate-admission.pkl","rb")
+    file = open("usa-graduate-admission.sav","rb")
     model = pickle.load(file)
     app.run()
